1b361cf9375fb1fa4ef0cf7513100a55e865d735
Chris Lawliss <chrislawliss@gmail.com>
2019-01-11 13:52:31 -0500

    changes months to days

--- models/marts/order_calculations_sm.sql
+++ models/marts/order_calculations_sm.sql
@@ -6,9 +6,9 @@ with orders as (
 prev_orders as (
     
     select 
-        orders.order_id,
-        lag(created_at, 1) over (partition by customer_id order by created_at) as prev_order_date,
-        datediff(month, prev_order_date, orders.created_at) as months_since_prev_order
+        orders.*,
+        lag(created_at) over (partition by customer_id order by created_at) as prev_order_date,
+        datediff(day, prev_order_date, orders.created_at) as days_since_prev_order
     from orders
     
 )
